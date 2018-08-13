1a1fc3fe825a979776356136eb3f05e72390edb9
mikescar <mike@tenthpeak.us>
2018-08-13 14:03:01 -0600

    Bypass need for prod.secret.exs

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :airdrop_interface, AirdropInterfaceWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "hidden-island-62242.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
