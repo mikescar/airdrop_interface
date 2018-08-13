b94650a1374c012e9013ce531307ae8faa046e0b
mikescar <mike@tenthpeak.us>
2018-08-13 14:06:25 -0600

    Use ENV for prod secrets

--- config/prod.exs
+++ config/prod.exs
@@ -17,8 +17,7 @@ config :airdrop_interface, AirdropInterfaceWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "hidden-island-62242.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -60,3 +59,7 @@ config :logger, level: :info
 #
 #     config :airdrop_interface, AirdropInterfaceWeb.Endpoint, server: true
 #
+
+# Finally import the config/prod.secret.exs
+# which should be versioned separately.
+import_config "prod.secret.exs"
